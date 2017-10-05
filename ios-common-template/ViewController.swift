a6f92d47e01af8a36fe202d67fb34f1dcb060ca6
dagi12 <dagi12@o2.pl>
2017-10-05 21:05:09 +0200

    Initial Commit

---
+++ ios-common-template/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-common-template
+//
+//  Created by Eryk Mariankowski on 05.10.2017.
+//  Copyright © 2017 Eryk Mariankowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
